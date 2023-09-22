ffd6c45ce1476de1ba8fd02172053338d3d494bc
Eric Hess <github.com@e-hess.com>
2023-09-22 03:13:55 +0200

    fixed issue that real env variables where not used because of php-fpm `clear_env`

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+
+ENV_FILENAME="/opt/app/.env"
+
+function getValue(){
+  if [ $# -ne 2 ]; then
+    echo "Error: expected 2 parameters"
+    exit 1
+  fi
+
+  local P_NAME="$1"
+  local P_DEFAULT="$2"
+  local value="${P_DEFAULT}"
+
+  if [ "${!P_NAME:-}" ]; then
+    value="${!P_NAME:-}"
+  fi
+
+  export $P_NAME=$value
+  echo "$value"
+}
+
+echo "APP_ENV=$(getValue APP_ENV prod)" > $ENV_FILENAME
+echo "APP_SECRET=$(getValue APP_SECRET 'change-me')" >> $ENV_FILENAME
+echo "DATABASE_URL=$(getValue DATABASE_URL 'sqlite:///%kernel.project_dir%/var/database/data.db')" >> $ENV_FILENAME
+echo "APP_ENABLE_REGISTRATION=$(getValue APP_ENABLE_REGISTRATION true)" >> $ENV_FILENAME
+
 php82 /opt/app/bin/console doctrine:migrations:migrate --no-interaction
 
 chmod 777 -R /opt/app/var
